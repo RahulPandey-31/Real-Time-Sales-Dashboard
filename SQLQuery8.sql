2a3cebada05ef2dbe8eee635699d91be80989118
Rahul Pandey <154305521+RahulPandey-31@users.noreply.github.com>
2024-06-16 21:42:39 +0530

    Add files via upload

---
+++ SQLQuery8.sql
@@ -0,0 +1,5 @@
+insert into sales_person values ('11','Aarav','Sharma')
+insert into sales_person values ('12','Ananya','Patel')
+insert into sales_person values ('13','Priya','Gupta')
+insert into sales_person values ('14','Rohan','Iyer')
+insert into sales_person values ('15','Sneha','Singh')
---
+++ SQLQuery9.sql
@@ -0,0 +1,3 @@
+insert into order_type values ('1','Online')
+insert into order_type values ('2','Counter')
+insert into order_type values ('3','Phone')
